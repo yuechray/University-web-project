966c9e5313e0d4c11eac5764cc7f57ce85513b3d
Ситников Даниил <den747f@gmail.com>
2025-04-02 16:50:34 +0300

    slick-slider

---
+++ app/static/js/carousel.js
@@ -0,0 +1,5 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
---
+++ static/js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        autoplay: true,  
+        autoplaySpeed: 3000,  
+        dots: true,  
+        arrows: true  
+    });
+});
